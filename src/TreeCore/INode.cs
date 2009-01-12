b6cf6d84b2bac874387d1207a44e03c421c78120
SND\Dykam_cp <SND\Dykam_cp@8ead0314-7f71-49e1-95c8-3147638646d4>
2009-01-12 16:07:38 +0000

    Started at the base. Created INode.cs and INodeList.cs

---
+++ src/TreeCore/INode.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreeCore
+{
+    interface INode
+    {
+        public INodeList Parents;
+        public INodeList Nodes;
+    }
+}
---
+++ src/TreeCore/INodeList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreeCore
+{
+    interface INodeList
+    {
+        public static INodeList operator +(INodeList nodeList, INodeList inodesToAdd);
+        public static INodeList operator -(INodeList nodeList, INodeList inodesToRemove);
+        public void NextNode();
+        public void PreviousNode();
+        public INode CurrentNode;
+    }
+}
